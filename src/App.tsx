a1385dc14b971b2e2dd9f9bdce650d4cb0835684
thibaultfradet <fradet.thibault23@gmail.com>
2026-02-01 21:24:15 +0100

    Feat : routing and authenticated layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "@/pages/Login";
+import Layout from "@/components/Layout";
+import Dashboard from "@/pages/Dashboard";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        {/* Routes protégées avec Layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
