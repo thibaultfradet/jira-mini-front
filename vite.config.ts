5c8092ca8fe5fbafefdfb91feb75f4b008544ec0
thibaultfradet <fradet.thibault23@gmail.com>
2026-06-08 13:58:06 +0200

    chore: remove Vite dev proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,31 +4,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-//Proxy config for dev environment
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://localhost",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/password": {
-        target: "https://localhost",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/auth": {
-        target: "https://localhost",
-        changeOrigin: true,
-        secure: false,
-      },
     },
   },
 })
